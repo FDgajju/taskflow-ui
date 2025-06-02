0dffa72609f63d40fecc9757c44430ad47ad6a78
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-06-03 01:34:27 +0530

    add: className and forward ref to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,33 +1,30 @@
-import type React from "react";
+import React from "react";
 
 type ButtonProps = {
   text?: string;
   type: "button" | "submit" | "reset";
   style?: string;
+  className?: string;
   onClick: () => void;
   children?: React.ReactNode;
-  reference?: React.RefObject<HTMLButtonElement | null>;
 };
 
-const Button: React.FC<ButtonProps> = ({
-  text,
-  type,
-  style,
-  onClick,
-  children,
-  reference,
-}) => {
-  return (
-    <button
-      ref={reference}
-      type={type}
-      className={`cursor-pointer bg-btn-primary py-2 px-4 text-btn-primary-text font-bold rounded-lg text-sm ${style}`}
-      onClick={onClick}
-      // disabled={true}
-    >
-      {text || children || "Submit"}
-    </button>
-  );
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ text, type, style, className, onClick, children }, ref) => {
+    return (
+      <button
+        ref={ref}
+        type={type}
+        className={`cursor-pointer bg-btn-primary py-2 px-4 text-btn-primary-text font-bold rounded-lg text-sm ${
+          style || className
+        }`}
+        onClick={onClick}
+        // disabled={true}
+      >
+        {text || children || "Submit"}
+      </button>
+    );
+  }
+);
 
 export default Button;
