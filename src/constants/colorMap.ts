964a6ebd586f366ac77bef7ebbb19cf55d8b4532
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-06-03 01:38:41 +0530

    meshing up color classes

--- src/constants/colorMap.ts
+++ src/constants/colorMap.ts
@@ -1,14 +1,22 @@
 export const colorClassMapTaskStatus: Record<string, string> = {
-	"btn-primary": "bg-primary-bg",
-	"status-todo": "bg-status-todo",
-	"status-inprogress": "bg-status-progress",
-	"status-done": "bg-status-done",
-	"status-overdue": "bg-status-overdue",
+  primary: "bg-primary-bg",
+  todo: "bg-status-todo",
+  inprogress: "bg-status-progress",
+  overdue: "bg-status-overdue",
+  done: "bg-status-done",
+
+  todo_secondary: "bg-status-todo-secondary",
+  inprogress_secondary: "bg-status-progress-secondary",
+  done_secondary: "bg-status-done-secondary",
+  overdue_secondary: "bg-status-overdue-secondary",
+};
+
+export const getColorClass = (property?: string, cc: string = "primary-bg") => {
+  return `${property}-${cc}`;
 };
 
 export const colorClassMapTaskPriority: Record<string, string> = {
-	high: "bg-priority-high",
-	medium: "bg-priority-medium",
-	low: "bg-priority-low",
+  high: "bg-priority-high",
+  medium: "bg-priority-medium",
+  low: "bg-priority-low",
 };
-    
