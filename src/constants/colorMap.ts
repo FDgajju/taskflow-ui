0cb4e6b5e63937d081dff88c979d0ca0bfda5455
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-06-04 11:24:01 +0530

    add: chaotic color class maps

--- src/constants/colorMap.ts
+++ src/constants/colorMap.ts
@@ -11,12 +11,41 @@ export const colorClassMapTaskStatus: Record<string, string> = {
   overdue_secondary: "bg-status-overdue-secondary",
 };
 
+export const colorClassMapTaskStatusText: Record<string, string> = {
+  primary: "text-primary-text",
+  todo: "text-status-todo",
+  inprogress: "text-status-progress",
+  overdue: "text-status-overdue",
+  done: "text-status-done",
+
+  todo_secondary: "text-status-todo-secondary",
+  inprogress_secondary: "text-status-progress-secondary",
+  done_secondary: "text-status-done-secondary",
+  overdue_secondary: "text-status-overdue-secondary",
+};
+
 export const getColorClass = (property?: string, cc: string = "primary-bg") => {
   return `${property}-${cc}`;
 };
 
 export const colorClassMapTaskPriority: Record<string, string> = {
   high: "bg-priority-high",
+  high_secondary: "bg-priority-high-secondary",
+
   medium: "bg-priority-medium",
+  medium_secondary: "bg-priority-medium-secondary",
+
   low: "bg-priority-low",
+  low_secondary: "bg-priority-low-secondary",
+};
+
+export const colorClassMapTaskPriorityText: Record<string, string> = {
+  high: "text-priority-high",
+  high_secondary: "text-priority-high-secondary",
+
+  medium: "text-priority-medium",
+  medium_secondary: "text-priority-medium-secondary",
+
+  low: "text-priority-low",
+  low_secondary: "text-priority-low-secondary",
 };
