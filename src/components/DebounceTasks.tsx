f7dfaa3a2f7a57ab37e3def8bd962da4fbb2ea9e
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-06-09 00:51:36 +0530

    added debounce task list component

---
+++ src/components/DebounceTasks.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import type { TaskT } from "../types/task";
+import HightedText from "./HightedText";
+import {
+  colorClassMapTaskStatus,
+  colorClassMapTaskStatusText,
+} from "../constants/colorMap";
+import Loader from "./Loader";
+
+const DebounceTasks: React.FC<{
+  tasks: TaskT[];
+  handleClick: (id: string) => void;
+  loading: boolean;
+}> = ({ tasks, handleClick, loading }) => {
+  return (
+    <div className="absolute bottom-16 w-full z-10 max-h-80 overflow-y-auto text-main border-1 border-btn-primary/50 drop-shadow-md drop-shadow-main/40 bg-secondary-bg rounded-xl p-3 flex flex-col gap-1">
+      {!loading && !tasks.length && (
+        <div className="flex justify-center items-center">
+          <span className="text-gray-text font-bold">No data found!</span>
+        </div>
+      )}
+
+      {loading && (
+        <div className="flex justify-center items-center">
+          <Loader />
+        </div>
+      )}
+      {!loading &&
+        tasks.map((task) => (
+          <div
+            onClick={() => handleClick(task._id)}
+            key={task._id}
+            className="text-main font-semibold py-2 px-2 hover:rounded-md hover:bg-sidebar-selected flex gap-2 items-center justify-between text-sm border-b-1 border-main/20 transition-all duration-200"
+          >
+            <div className="flex items-center w-full gap-3 ">
+              <HightedText
+                text={task.ticket as string}
+                className="text-sm bg-main/20 py-0.5 "
+              />
+              <p className="w-1/2 text-start">{task.title}</p>
+            </div>
+            <HightedText
+              text={task.status as string}
+              className={`text-sm ${
+                colorClassMapTaskStatus[`${task.status}_secondary`]
+              } ${colorClassMapTaskStatusText[task.status]} py-0.5 `}
+            />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default DebounceTasks;
