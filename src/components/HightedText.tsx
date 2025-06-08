66994ebee0e9621e44f9c9937420c3e57665b068
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-06-09 00:52:15 +0530

    refactor

--- src/components/HightedText.tsx
+++ src/components/HightedText.tsx
@@ -3,12 +3,17 @@ import type React from "react";
 type HighlightedTextProps = {
   text: string;
   style?: string;
+  className?: string;
 };
 
-const HightedText: React.FC<HighlightedTextProps> = ({ text, style }) => {
+const HightedText: React.FC<HighlightedTextProps> = ({
+  text,
+  style,
+  className,
+}) => {
   return (
     <span
-      className={`bg-gray-text p-1 rounded-md text-primary-bg font-bold ${style}`}
+      className={`bg-gray-text p-1 rounded-md text-primary-bg font-bold ${style} ${className}`}
     >
       {text}
     </span>
